ae11d42c277f724717cc567c8ee02029fe206d56
Federico Peralta <peralta.federico.manuel@gmail.com>
2020-10-24 16:26:25 -0300

    add movie feedback

--- src/components/Layout/AddMovieModal/AddMovieModal.tsx
+++ src/components/Layout/AddMovieModal/AddMovieModal.tsx
@@ -1,8 +1,13 @@
 import React, { ReactElement, useState } from 'react'
+import { isEmpty } from 'lodash'
 import * as Styled from './AddMovieModal.styles'
 import AddMovieForm from './AddMovieForm/AddMovieForm'
 import AddMovieSuccess from './AddMovieSuccess/AddMovieSuccess'
 
+interface Movie {
+  name: string
+  category: string
+}
 interface Props {
   onClose: () => void
   show: boolean
@@ -12,18 +17,18 @@ interface Props {
 // TODO: Add types
 
 export default function AddMovieModal({ onClose, show }: Props): ReactElement {
-  const [success, setSuccess] = useState(false)
+  const [movie, setMovie] = useState({} as Movie)
 
-  const handlePublishMovie = (values: any) => {
-    setSuccess(true)
-  }
+  const handlePublishMovie = (values: Movie) => setMovie(values)
+
+  const success = !isEmpty(movie)
 
   return (
     <Styled.Modal onClose={onClose} show={show} success={success}>
       {success ? (
         <AddMovieSuccess
-          movie="Liteflix The Movie"
-          category="Aventuras"
+          movie={movie.name}
+          category={movie.category}
           onClose={onClose}
         />
       ) : (
