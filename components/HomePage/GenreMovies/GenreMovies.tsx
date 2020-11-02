7740b482e89584a7c74f4f8caf8e68eb8d713149
Federico Peralta <peralta.federico.manuel@gmail.com>
2020-11-01 21:33:25 -0300

    add types

--- components/HomePage/GenreMovies/GenreMovies.tsx
+++ components/HomePage/GenreMovies/GenreMovies.tsx
@@ -1,6 +1,12 @@
 import React, { ReactElement } from 'react'
+import { Movie } from '../../../interfaces'
 import MovieCategory from '../../commons/MovieCategory/MovieCategory'
 
-export default function GenreMovies({ movies, title }: any): ReactElement {
+interface Props {
+  movies: Movie[]
+  title: string
+}
+
+export default function GenreMovies({ movies, title }: Props): ReactElement {
   return <MovieCategory title={title} size="wide" movies={movies} />
 }
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -10,3 +10,37 @@ export type User = {
 }
 
 export type CardSize = 'large' | 'wide'
+
+export interface Movie {
+  title: string
+  genre: string
+  imgUrl: string
+}
+
+export interface MainMovies {
+  featured: Movie & { overview: string }
+  upcoming: Movie[]
+  popular: Movie[]
+}
+
+export interface GroupedByGenreMovie {
+  movies: Movie[]
+  genre: string
+}
+
+export interface Genre {
+  id: number
+  name: string
+}
+
+export interface RawMovie {
+  tmdbGenreId: number
+  imgUrl: string
+  title: string
+}
+
+export interface RawMovieForm {
+  tmdbGenreId: string
+  imgUrl: string
+  title: string
+}
